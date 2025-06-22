43d5845bde7779f5d260b1b0eb7642dc4dbda4f9
peiich <peggyw0206@gmail.com>
2025-06-22 23:59:26 +0800

    Add website files

---
+++ 1111771/1111771/Controllers/HW1Controller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _1111771.Controllers
+{
+    public class hw1Controller : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult prog1(int columns, int rows, string text)
+    {
+        ViewBag.Columns = columns;
+        ViewBag.Rows = rows;
+        ViewBag.Text = text;
+        return View();
+    }
+}
+}
